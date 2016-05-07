4d950e1affa18f639876b6f33865211681ca6b08
colining <18810782954@163.com>
2016-05-07 14:30:27 +0800

    加了记住密码的登录界面

--- app/src/main/java/com/example/asus/broadcastbestpractice/LoginActivity.java
+++ app/src/main/java/com/example/asus/broadcastbestpractice/LoginActivity.java
@@ -1,10 +1,13 @@
 package com.example.asus.broadcastbestpractice;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
+import android.preference.PreferenceManager;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.Toast;
 
@@ -15,6 +18,10 @@ public class LoginActivity extends BaseActivity {
     private EditText accountEdit;
     private EditText passwordEdit;
     private Button login;
+
+    private SharedPreferences pref;
+    private SharedPreferences.Editor editor;
+    private CheckBox rememberpass;
     @Override
     protected void  onCreate(Bundle savedInstanceState)
     {
@@ -23,12 +30,34 @@ public class LoginActivity extends BaseActivity {
         accountEdit =(EditText)findViewById(R.id.account);
         passwordEdit = (EditText) findViewById(R.id.password);
         login=(Button)findViewById(R.id.login);
+        pref = PreferenceManager.getDefaultSharedPreferences(this);
+        rememberpass= (CheckBox) findViewById(R.id.remember_pass);
+        boolean isremember =pref.getBoolean("remember_password",false);
+        if(isremember){
+            String account =pref.getString("account","");
+            String password= pref.getString("password","");
+            accountEdit.setText(account);
+            passwordEdit.setText(password);
+            rememberpass.setChecked(true);
+        }
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 String account =accountEdit.getText().toString();
                 String password =passwordEdit.getText().toString();
                 if(account.equals("admin")&&password.equals("123456")){
+                    editor =pref.edit();
+                    if(rememberpass.isChecked()){
+                        editor.putBoolean("remember_password",true);
+                        editor.putString("account",account);
+                        editor.putString("password",password);
+
+                    }
+                    else
+                    {
+                        editor.clear();
+                    }
+                    editor.commit();
                     Intent intent =new Intent(LoginActivity.this,MainActivity.class);
                     startActivity(intent);
                     finish();
